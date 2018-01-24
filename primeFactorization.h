9f01771c043b923c45e04b469e087434745cad63
Jeffrey <jeffrey.darling15@gmail.com>
2018-01-23 18:03:03 -0800

    Update primeFactorization.h

--- primeFactorization.h
+++ primeFactorization.h
@@ -1,66 +1,57 @@
 #ifndef primeFactorization_H
 #define primeFactorization_H
 
-#include "integerManipulation.h"
-
-class primeFactorization: public integerManipulation
+//#include "integerManipulation.h"
+using namespace std;
+class primeFactorization//: public integerManipulation
 {
 public:
 
-       void factorization();
+void factorization(int n){
       //Function to output the prime factorization of num 
       //Postcondition: Prime factorization of num is printed;
+ // Print the number of 2s that divide n
+    while (n%2 == 0)
+    {
+        printf("%d ", 2);
+        n = n/2;
+    }
 
-       void primeFactorization(long long n = 0);
+    // n must be odd at this point.  So we can skip 
+    // one element (Note i = i +2)
+    for (int i = 3; i <= sqrt(n); i = i+2)
+    {
+        // While i divides n, print i and divide n
+        while (n%i == 0)
+        {
+            printf("%d ", i);
+            n = n/i;
+        }
+    }
+
+    // This condition is to handle the case when n 
+    // is a prime number greater than 2
+    if (n > 2)
+        printf ("%d ", n);
+}
+void setnum(){
+cout << "Enter an integer between 2 and 270,000,000,000,000: ";
+cin >> n;
+}
+int getnum(){
+return n;
+}
+  //  primeFactorization(long long n = 0);
       //Constructor with a default parameter.
       //The instance variables of the base class are set according 
       //to the parameters and the array first125000Primes is 
       //created.
-      //Postcondition: num = n; revNum = 0; evenscount = 0;
+     //Postcondition: num = n; revNum = 0; evenscount = 0;
       //    oddsCount = 0; zerosCount = 0;
       //    first125000Primes = first 125000 prime numbers.      
-        primeFactorization::primeFactorization(long long n) : integerManipulaiton(n)
-        {
-                first125000PrimeNum(first125000Primes, 125000);
-        }//end constructor
-        void primefactorization::factorization()
-        {
-                long long original = integerManipulaiton::getNum();
-                first125000PrimeNum(first125000Primes, 125000);
-                if(first125000PrimeNum[0] == original)
-                cout << original << "is a prime number.",
-                else
-                cout << original << "is not a prime number.";
-                cout << "Its factorization is." << endl;
-                cout << original << "=";
-                for (int i =0,i < 125000, i++)
-                if(first125000Primes[] !=-1)
-                {
-                        cout << first125000Primes[];
-                        if(i != 125000-1 && first125000Primes[i+1] != -1)
-                                cout <<"*";
-                        else
-                        break;
-                }//end for
-                cout << endl;
-        }// end factorization function
-        void primeFactorization::first125000PrimeNum(long long list[], int length)
-        {
-                for(int i=0, i < length, i++)
-                        list[]=-1;
-                long long number= integerManipulaiton::getNum();
-                int index = 0;
-                for(long long i = 2, i <= number, i++)
-                {
-                        while (number % i ==0)
-                        {
-                                list[index] = i;
-                                index++;
-                                number=number / i;
-                        }//end while
-                }//end for
-        }//end of first125000PrimeNum function
+
 private:
+int n;
     long long first125000Primes[125000];
 
     void primeFact(long long num, long long list[], int length,
@@ -69,7 +60,7 @@ private:
       //Function to determine and store the first 125000 prime 
       //integers.
       //Postcondition: The first 125000 prime numbers are  
-      //    determined and stored in the array first125000Primes;
+    //      determined and stored in the array first125000Primes;
 
     void primeTest(long long num, long long list[], int length,
                    bool& primeNum, int& firstPrimeFactIndex);
